fc0c078bae619d87e89ae38f5f69cf5bf580c783
Kim Richards <richards.kimc@gmail.com>
2025-05-29 20:55:07 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World! By Kim Richards"); // New line appended
+        Console.Write("Hello! From Kim"); // No new line appended    
+    }
+  }
+}
