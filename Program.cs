577f4bac78a99199c44fea1db79ee9e22f262847
Kim Richards <richards.kimc@gmail.com>
2025-06-02 20:31:14 +0100

    Commented out if/else statement example

--- Program.cs
+++ Program.cs
@@ -316,55 +316,55 @@ using System.Diagnostics;
 //     }
 // }
 
-// Uncomment for else if statement example
-namespace HelloWorld
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Random dice = new Random();
+// // Uncomment for else if statement example
+// namespace HelloWorld
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Random dice = new Random();
 
-            int roll1 = dice.Next(1, 7);
-            int roll2 = dice.Next(1, 7);
-            int roll3 = dice.Next(1, 7);
+//             int roll1 = dice.Next(1, 7);
+//             int roll2 = dice.Next(1, 7);
+//             int roll3 = dice.Next(1, 7);
 
-            int total = roll1 + roll2 + roll3;
+//             int total = roll1 + roll2 + roll3;
 
-            Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+//             Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-            if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
-            {
-                if ((roll1 == roll2) && (roll2 == roll3))
-                {
-                    Console.WriteLine("You rolled triples!  +6 bonus to total!");
-                    total += 6;
-                }
-                else
-                {
-                    Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-                    total += 2;
-                }
+//             if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+//             {
+//                 if ((roll1 == roll2) && (roll2 == roll3))
+//                 {
+//                     Console.WriteLine("You rolled triples!  +6 bonus to total!");
+//                     total += 6;
+//                 }
+//                 else
+//                 {
+//                     Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+//                     total += 2;
+//                 }
 
-                Console.WriteLine($"Your total including the bonus: {total}");
-            }
+//                 Console.WriteLine($"Your total including the bonus: {total}");
+//             }
 
-            if (total >= 16)
-            {
-                Console.WriteLine("You won a car!");
-            }
-            else if (total >= 10)
-            {
-                Console.WriteLine("You won a holiday");
-            }
-            else if (total >= 5)
-            {
-                Console.WriteLine("You won a bike!");
-            }
-            else
-            {
-                Console.WriteLine("You won £5");
-            }
-        }
-    }
-}
+//             if (total >= 16)
+//             {
+//                 Console.WriteLine("You won a car!");
+//             }
+//             else if (total >= 10)
+//             {
+//                 Console.WriteLine("You won a holiday");
+//             }
+//             else if (total >= 5)
+//             {
+//                 Console.WriteLine("You won a bike!");
+//             }
+//             else
+//             {
+//                 Console.WriteLine("You won £5");
+//             }
+//         }
+//     }
+// }
